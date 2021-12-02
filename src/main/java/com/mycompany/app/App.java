7fcd101c4ce3df7466220f8a9f7f30cace0a91f4
Augusto Morante <augustomorante@hotmail.com>
2021-12-02 17:40:38 -0500

    add-message in spanish

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,20 +2,29 @@ package com.mycompany.app;
 
 /**
  * Hello world!
+ * Hola Mundo!
  */
 public class App
 {
 
     private final String message = "Hello World!";
 
+    private final String messageSpanish = "Hola mundo!";
+
     public App() {}
 
     public static void main(String[] args) {
+
         System.out.println(new App().getMessage());
+        System.out.println(new App().getMessageSpanish());
     }
 
     private final String getMessage() {
         return message;
+    }
+
+    private final String getMessageSpanish() {
+        return messageSpanish;
     }
 
 }
